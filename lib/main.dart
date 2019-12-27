7e6dbe22664740367114d260f085e5207134b01a
Spiler159357 <6577kys@naver.com>
2019-12-27 21:39:42 +0900

    숫자 제거

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo1:31231231231',
+      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.123123132
         //
